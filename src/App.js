ecad294e8df40e0367898e6b069f308486e7e97b
spaceforestchu <spaceforestchu@gmail.com>
2023-04-26 19:01:35 -0400

    playing with env variables

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function App() {
   console.log(process.env);
   console.log(process.env.NODE_ENV);
 
-  return <div className="App">current ENV = {process.env.NODE_ENV}</div>;
+  return (
+    <div className="App">
+      <p>current ENV = {process.env.NODE_ENV}</p>
+      Current Env is{" "}
+      {process.env.NODE_ENV === "development" ? "development" : "production"} so
+      we are using:{" "}
+      {process.env.NODE_ENV === "development"
+        ? process.env.REACT_APP_API_KEY
+        : process.env.REACT_APP_OTHER_API_KEY}
+    </div>
+  );
 }
 
 export default App;
