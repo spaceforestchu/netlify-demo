cf8969f8c48c4953635598209e59f43f45cb558a
spaceforestchu <spaceforestchu@gmail.com>
2023-04-26 18:56:15 -0400

    added .env

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> App</div>;
+  console.log(process.env);
+  console.log(process.env.NODE_ENV);
+
+  return <div className="App">current ENV = {process.env.NODE_ENV}</div>;
 }
 
 export default App;
